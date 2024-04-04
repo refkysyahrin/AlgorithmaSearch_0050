dc29a352f5a7b98c1bc3bc9e591616a2c8336d22
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-04 17:16:53 +0700

    cout

--- AlgorithmaSearch_0050.cpp
+++ AlgorithmaSearch_0050.cpp
@@ -21,6 +21,9 @@ void input()
 	cout << "\n------------------------\n";
 	cout << " Enter array elements\n";
 	cout << "------------------------\n";
-	for (i = 0; i <n; i++)
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
 
+	}
 }
