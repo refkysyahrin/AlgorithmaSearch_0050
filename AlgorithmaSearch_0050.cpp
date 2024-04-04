23c104a0ec1bc555012f69fa43c3024948955fcd
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-04 17:22:37 +0700

    step 2

--- AlgorithmaSearch_0050.cpp
+++ AlgorithmaSearch_0050.cpp
@@ -38,5 +38,9 @@ void LinearSearch()
 	{
 		// Accept the number to be search
 		cout << "\nEnter the element you want to search: "; // step 1
+		cin >> item;
+
+		ctr = 0;
+		i = 0; // step 2
 	}
 }
