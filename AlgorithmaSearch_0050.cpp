809558985567cc4fb588cb89865e86fcdd93a713
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-04 17:26:15 +0700

    step 6

--- AlgorithmaSearch_0050.cpp
+++ AlgorithmaSearch_0050.cpp
@@ -43,5 +43,13 @@ void LinearSearch()
 		ctr = 0;
 		i = 0; // step 2
 		while (i < n) // step 3
+		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << "Found at position " << (i + 1) << endl; // step 6
+
+			}
+		}
 	}
 }
